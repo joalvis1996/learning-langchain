d19c6a4fc063df5579e167122cad36c374135014
Minhyeok <tracertw@gmail.com>
2025-03-02 19:56:00 +0900

    Refine comments and improve code readability in 15.rag-colbert.py

--- python/ch02/15.rag-colbert.py
+++ python/ch02/15.rag-colbert.py
@@ -1,5 +1,5 @@
 """
-- Windows는 지원되지 않음. RAGatouille는 WSL 외부에서 작동하지 않으며 WSL1에서도 문제가 있음. 일부 사용자들은 WSL2에서 성공적으로 실행했다고 함.
+- Windows는 지원되지 않음.
 - 파이썬에서만 사용 가능함.
 - 전체 문서는 여기서 확인할 수 있음: https://github.com/AnswerDotAI/RAGatouille/blob/8183aad64a9a6ba805d4066dcab489d97615d316/README.md
 
@@ -18,11 +18,13 @@ RAG = RAGPretrainedModel.from_pretrained("colbert-ir/colbertv2.0")
 def get_wikipedia_page(title: str):
     """
     위키피디아 페이지의 전체 텍스트 내용을 가져옴.
+
     :param title: str - 위키피디아 페이지의 제목
     :return: str - 페이지의 전체 텍스트 내용을 raw 문자열로 반환
     """
     # 위키피디아 API 엔드포인트
     URL = "https://en.wikipedia.org/w/api.php"
+
     # API 요청을 위한 매개변수
     params = {
         "action": "query",
@@ -31,16 +33,20 @@ def get_wikipedia_page(title: str):
         "prop": "extracts",
         "explaintext": True,
     }
+
     # 위키피디아의 모범 사례를 준수하기 위한 사용자 정의 User-Agent 헤더
     headers = {"User-Agent": "RAGatouille_tutorial/0.0.1"}
+
     response = requests.get(URL, params=params, headers=headers)
     data = response.json()
+    
     # 페이지 내용 추출
     page = next(iter(data["query"]["pages"].values()))
     return page["extract"] if "extract" in page else None
 
 
 full_document = get_wikipedia_page("Hayao_Miyazaki")
+
 # 인덱스 생성
 RAG.index(
     collection=[full_document],
@@ -48,6 +54,7 @@ RAG.index(
     max_document_length=180,
     split_documents=True,
 )
+
 # 쿼리
 results = RAG.search(query="What animation studio did Miyazaki found?", k=3)
 
