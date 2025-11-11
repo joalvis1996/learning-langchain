0d4878b6656f470968006f1a2ce6c4277dcea495
alvisjo <lwalwakk1@naver.com>
2025-11-11 16:05:29 +0900

    주석 수정

--- 노트/others/04_Memory/01_ConversationalBufferMemory.py
+++ 노트/others/04_Memory/01_ConversationalBufferMemory.py
@@ -27,7 +27,7 @@ memory.save_context(
     }
 )
 
-# 메시지 히스토리 반환 함수
+# 저장된 메시지 히스토리 반환 함수
 print(memory.load_memory_variables({}))
 ## 실행 결과
 # {'history': 'Human: 안녕, 내 이름은 홍길동이야\nAI: 안녕하세요! 홍길동님, 무엇을 도와드릴까요?\nHuman: 양재에서 강남으로 가는 방법 알려줘\nAI: 140번 버스를 이용하시면 10분 내외로 갈 수 있습니다.'}
