18d28dc51d8dde03b5fe541a135986f5c87493e7
alvisjo <lwalwakk1@naver.com>
2025-11-11 16:11:51 +0900

    메시지 히스토리 불러오기 예제 수정

--- 노트/others/04_Memory/01_ConversationalBufferMemory.py
+++ 노트/others/04_Memory/01_ConversationalBufferMemory.py
@@ -1,5 +1,7 @@
 import json
+
 from dotenv import load_dotenv
+
 from langchain.memory import ConversationBufferMemory
 
 load_dotenv()
@@ -7,27 +9,29 @@ load_dotenv()
 with open("../ch_3_Model/geminiLLM.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
-memory = ConversationBufferMemory()
+memory = ConversationBufferMemory(return_messages=True)
+# memory = ConversationBufferMemory(return_messages=True)
 
 memory.save_context(
     inputs={
-"human": "안녕, 내 이름은 홍길동이야"
+        "human": "안녕, 내 이름은 홍길동이야"
     },
     outputs={
-"ai": "안녕하세요! 홍길동님, 무엇을 도와드릴까요?"
+        "ai": "안녕하세요! 홍길동님, 무엇을 도와드릴까요?"
     },
 )
 
 memory.save_context(
     inputs={
-"human": "양재에서 강남으로 가는 방법 알려줘"
+        "human": "양재에서 강남으로 가는 방법 알려줘"
     },
     outputs={
-"ai": "140번 버스를 이용하시면 10분 내외로 갈 수 있습니다."
+        "ai": "140번 버스를 이용하시면 10분 내외로 갈 수 있습니다."
     }
 )
 
 # 저장된 메시지 히스토리 반환 함수
 print(memory.load_memory_variables({}))
+
 ## 실행 결과
 # {'history': 'Human: 안녕, 내 이름은 홍길동이야\nAI: 안녕하세요! 홍길동님, 무엇을 도와드릴까요?\nHuman: 양재에서 강남으로 가는 방법 알려줘\nAI: 140번 버스를 이용하시면 10분 내외로 갈 수 있습니다.'}
