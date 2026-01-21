29516873f976525f0b9a2cdb804b9d7212dd9251
alvisjo <lwalwakk1@naver.com>
2026-01-21 18:31:40 +0900

    .

--- python/ch02/14.multi-vector-retriever.py
+++ python/ch02/14.multi-vector-retriever.py
@@ -31,6 +31,7 @@ chunks = splitter.split_documents(docs)
 # 나머지 코드는 동일하게 유지
 prompt_text = '다음 문서의 요약을 생성하세요:\n\n{doc}'
 
+
 prompt = ChatPromptTemplate.from_template(prompt_text)
 llm = ChatOpenAI(temperature=0, model='gpt-4o-mini')
 summarize_chain = {
