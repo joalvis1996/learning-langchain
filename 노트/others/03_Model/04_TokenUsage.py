1f529be7c5aa3bb429679afd0815ccd5c4760958
alvisjo <lwalwakk1@naver.com>
2026-01-28 23:51:44 +0900

    .

--- 노트/others/03_Model/04_TokenUsage.py
+++ 노트/others/03_Model/04_TokenUsage.py
@@ -7,13 +7,14 @@ load_dotenv()
 
 with open("geminiLLM.json", "r", encoding="utf-8") as f:
     data = json.load(f)
-    
+
 
 
 llm = load(data)
 
 response = llm.invoke("너는 누구니")
 print(response.usage_metadata)
+
 
 ## 실행 결과
 # {'input_tokens': 5, 'output_tokens': 13, 'total_tokens': 115, 'input_token_details': {'cache_read': 0}}
